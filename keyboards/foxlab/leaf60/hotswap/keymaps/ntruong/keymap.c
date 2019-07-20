9aa26a5359f8a6d862d8cb5c9b0ccb8cdde65c26
ntruong <nt31415@gmail.com>
2019-07-19 19:18:48 -0500

    removed macro codes and updated system controls

--- keyboards/foxlab/leaf60/hotswap/keymaps/ntruong/keymap.c
+++ keyboards/foxlab/leaf60/hotswap/keymaps/ntruong/keymap.c
@@ -3,19 +3,6 @@
 #undef  PRODUCT
 #define PRODUCT hhkb
 
-enum keycodes {
-  QWERTY = SAFE_RANGE,
-  DYNAMIC_MACRO_RANGE,
-};
-
-#include "dynamic_macro.h"
-
-#define RECORD1 DYN_REC_START1
-#define RECORD2 DYN_REC_START2
-#define REPLAY1 DYN_MACRO_PLAY1
-#define REPLAY2 DYN_MACRO_PLAY2
-#define STOPREC DYN_REC_STOP
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Layer 0: Default layer
@@ -23,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
    * │ ` │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │ \ │ ⎋ │
    * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┤
-   * │  ↹  │ ⒊ │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ [ │ ] │  ⌫  │
+   * │  ↹  │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ [ │ ] │  ⌫  │
    * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
    * │  ⎋⌃  │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ⒈ │ ' │   ↩    │
    * ├──────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴────┬───┤
@@ -34,21 +21,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *
    */
   [0] = LAYOUT_60_tsangan_hhkb(
-      KC_GRV,         KC_1,        KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,    KC_9,   KC_0,           KC_MINS, KC_EQL,  KC_BSLS, KC_ESC,
-      KC_TAB,         LT(3, KC_Q), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,           KC_LBRC, KC_RBRC, KC_BSPC,
-      LCTL_T(KC_ESC), KC_A,        KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   LT(1, KC_SCLN), KC_QUOT, KC_ENT,
-      KC_LSFT,        KC_Z,        KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,        KC_RSFT, MO(2),
+      KC_GRV,         KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,    KC_9,   KC_0,           KC_MINS, KC_EQL,  KC_BSLS, KC_ESC,
+      KC_TAB,         KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,           KC_LBRC, KC_RBRC, KC_BSPC,
+      LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   LT(1, KC_SCLN), KC_QUOT, KC_ENT,
+      KC_LSFT,        KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,        KC_RSFT, MO(2),
       KC_LALT, KC_NO, KC_LGUI, KC_SPC, KC_RGUI, KC_NO, KC_RALT
       ),
 
-  /* Layer 1: Default layer
+  /* Layer 1: Function, arrow, and system keys
    *
    * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
-   * │ ⟳ │ ⅰ │ ⅱ │ ⅲ │ ⅳ │ ⅴ │ ⅵ │ ⅶ │ ⅷ │ ⅸ │ ⅹ │ ⅺ │ ⅻ │   │   │
+   * │   │ ⅰ │ ⅱ │ ⅲ │ ⅳ │ ⅴ │ ⅵ │ ⅶ │ ⅷ │ ⅸ │ ⅹ │ ⅺ │ ⅻ │   │   │
    * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┤
-   * │     │   │ † │ ‡ │   │   │   │   │   │   │   │   │   │     │
+   * │     │   │   │ ⇲ │   │   │   │   │   │   │   │   │   │     │
    * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
-   * │      │   │   │   │   │   │ ← │ ↓ │ ↑ │ → │   │   │        │
+   * │      │ ⇱ │   │   │   │   │ ← │ ↓ │ ↑ │ → │   │   │        │
    * ├──────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴────┬───┤
    * │   ⇪   │   │   │   │   │   │   │   │   │   │   │       │   │
    * └───────┴─┬─┴─┬─┴─┬─┴───┴───┴───┴───┴───┴─┬─┴─┬─┴─┬─────┴───┘
@@ -57,17 +44,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *
    */
   [1] = LAYOUT_60_tsangan_hhkb(
-      RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
-      _______, _______, REPLAY1, REPLAY2, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
-      _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
+      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
+      _______, _______, _______, KC_END,  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
+      _______, KC_HOME, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
       KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       KC_VOLD, _______, KC_VOLU, KC_MPLY, _______, _______, _______
       ),
 
-  /* Layer 2: Various system controls layer
+  /* Layer 2: Miscellaneous system keys
    *
    * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
-   * │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │
+   * │ ⟳ │   │   │   │   │   │   │   │   │   │   │   │   │   │   │
    * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┤
    * │     │   │   │   │   │   │   │   │   │   │   │   │   │     │
    * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
@@ -75,39 +62,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ├──────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴────┬───┤
    * │       │   │   │   │   │   │   │   │   │   │   │       │   │
    * └───────┴─┬─┴─┬─┴─┬─┴───┴───┴───┴───┴───┴─┬─┴─┬─┴─┬─────┴───┘
-   *           │   │   │           ☼           │ ◐ │ ◑ │
+   *           │ ¤ │   │           ☼           │ ◐ │ ◑ │
    *           └───┴───┴───────────────────────┴───┴───┘
    *
    */
-  [1] = LAYOUT_60_tsangan_hhkb(
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  [2] = LAYOUT_60_tsangan_hhkb(
+      RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, BL_TOGG, BL_DEC,  _______, BL_INC
-      ),
-
-  /* Layer 3: Macro layer
-   *
-   * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
-   * │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │
-   * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┤
-   * │  ∅  │ ∅ │ † │ ‡ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │  ∅  │
-   * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
-   * │  ∅   │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │   ∅    │
-   * ├──────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴────┬───┤
-   * │   ∅   │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │ ∅ │   ∅   │ ∅ │
-   * └───────┴─┬─┴─┬─┴─┬─┴───┴───┴───┴───┴───┴─┬─┴─┬─┴─┬─────┴───┘
-   *           │ ∅ │ ∅ │           ◙           │ ∅ │ ∅ │
-   *           └───┴───┴───────────────────────┴───┴───┘
-   *
-   */
-  [3] = LAYOUT_60_tsangan_hhkb(
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, RECORD1, RECORD2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, STOPREC, XXXXXXX, XXXXXXX, XXXXXXX
+      BL_BRTG, _______, _______, BL_TOGG, BL_DEC,  _______, BL_INC
       )
 };
 
@@ -119,9 +83,3 @@ void matrix_init_user(void) { }
 void matrix_scan_user(void) { }
 
 void led_set_user(uint8_t usb_led) { }
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record)
-{
-  if (!process_record_dynamic_macro(keycode, record)) { return false; }
-  return true;
-}
